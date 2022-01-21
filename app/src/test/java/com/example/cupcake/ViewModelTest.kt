6c3a460f0a2e7c51b6969f05020cde3db5bc6cc8
Ramon Felix <ramon.felix@zeiss.com>
2022-01-21 10:44:57 -0800

    finishing test lab

--- app/src/test/java/com/example/cupcake/ViewModelTest.kt
+++ app/src/test/java/com/example/cupcake/ViewModelTest.kt
@@ -3,9 +3,9 @@ package com.example.cupcake
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.example.cupcake.model.OrderViewModel
 import org.junit.Assert.assertEquals
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-
 
 
 class ViewModelTests {
@@ -13,18 +13,28 @@ class ViewModelTests {
     @get:Rule
     var instantTaskExecutorRule = InstantTaskExecutorRule()
 
+    private lateinit var viewModel: OrderViewModel
+
+    @Before
+    fun setup() {
+        viewModel = OrderViewModel()
+    }
+
     @Test
     fun quantity_twelve_cupcakes() {
-        val viewModel = OrderViewModel()
         viewModel.setQuantity(12)
         assertEquals(12, viewModel.quantity.value)
     }
 
     @Test
     fun price_twelve_cupcakes() {
-        val viewModel = OrderViewModel()
-        viewModel.setQuantity(12)
+        viewModel.setQuantity(6)
         viewModel.price.observeForever {}
-        assertEquals("$27.00", viewModel.price.value)
+        assertEquals("$15.00", viewModel.price.value)
+    }
+    @Test
+    fun delivery_date(){
+        viewModel.setDate("01/21/2022")
+        assertEquals("01/21/2022",viewModel.date.value)
     }
 }
